ced9c6d9cd4068d44464aa62c627c9d6f0e444b1
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 17:16:46 +0000

    fix: corregir uso de LocalDateTime en la clase Cliente

--- entregas/ponceKevin/src/Cliente.java
+++ entregas/ponceKevin/src/Cliente.java
@@ -1,12 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Cliente {
     public static void main(String[] args) {
         Liga unea = new Liga("UNEA santander");
         Equipo pelatlantico = new Equipo("fc-pelatlántico", "rojo");
         Equipo burger = new Equipo("Burgeger", "verde");
         Equipo geek = new Equipo("geek", "negro");
-        Partido geekXBurger = new Partido(geek, burger, new LocalDateTime(2026, 6, 30, 15, 0));
+        Partido geekXBurger = new Partido(geek, burger, LocalDateTime.of(2026, 6, 30, 15, 0));
         unea.programarPartido(geekXBurger);
-        Partido  pelatlanticoXgeek = new Partido(pelatlantico, geek, new LocalDateTime(2024, 7, 1, 15, 0));
+        Partido  pelatlanticoXgeek = new Partido(pelatlantico, geek, LocalDateTime.of(2024, 7, 1, 15, 0));
         unea.programarPartido(pelatlanticoXgeek);
         pelatlanticoXgeek.marcarComoCompleto();
 
