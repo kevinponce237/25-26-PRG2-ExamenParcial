26016cdc718cf2f006fbc902a744b7072cef6a9b
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 13:53:35 +0000

    se replico la vista publica de partido

---
+++ entregas/ponceKevin/src/Partido.java
@@ -0,0 +1,42 @@
+public class Partido {
+    partido(Equipo equipo1, Equipo equipo2,LocalDateTime fecha) {
+        
+    }
+
+    void aumentarGoles(int numeroEquipo, int cantidDeGoles) {
+        
+    }
+
+    void aumentarGoles(int numeroEquipo) {
+        
+    }
+
+    boolean pasoLaFecha() {
+     
+    }
+
+    boolean estaPendiente() {
+        
+    }
+
+    Equipo getGanador() {
+        
+    }
+
+    int[] getGolesPorEquipo() {
+        
+    }
+
+    int diasFaltantes() {
+        
+    }
+
+    int diasFaltantes(LocalDateTime fechaAComparar) {
+        
+    }
+
+    String toString() {
+        
+    }
+
+}
