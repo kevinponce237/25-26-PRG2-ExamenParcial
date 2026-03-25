620dd02916cf784c50fcd8c6a36388e446656807
Kevin David Ponce Machigua <kevin.ponce@alumnos.uneatlantico.es>
2026-03-25 13:33:18 +0000

    replicar vista publica del equipo

---
+++ entregas/ponceKevin/src/Equipo.java
@@ -0,0 +1,26 @@
+public class Equipo {
+
+
+    public Equipo(String nombre, String ciudad) {
+
+    }
+
+    public void aumentarVictorias() {
+
+    }
+
+    public void aumentarDerrotas() {
+
+    }
+    public void aumentarEmpates() {
+
+    }
+
+    public void actualizarTotal(int puntos) {
+
+    }
+
+    public String toString() {
+      
+    }
+}
